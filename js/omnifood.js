441995c64cc7fbd715c695eeb4e8c005ca6ac7b9
Harm Kolvoort <hkolvoort@gmail.com>
2025-08-28 22:46:22 +0200

    building omnifood making mobile nav work

---
+++ js/omnifood.js
@@ -0,0 +1,30 @@
+console.log("omnifood loaded");
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+  
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+  
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+  
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// Make mobile nav work
+
+"nav-open"
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", (e) => {
+  headerEl.classList.toggle("nav-open");
+})
